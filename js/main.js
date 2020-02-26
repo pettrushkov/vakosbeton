52959e2a4afb431d1cc2458f1a454f15b6e921c3
Denys Petrushkov <denis.p@cheitgroup.com>
2020-02-26 20:41:34 +0200

    completed desctop fullhd without layer on map section

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
 jQuery(document).ready(function($) {
     
    $('.header__slider').slick({
-       arrows: true
+       arrows: true,
+       asNavFor: '.header__text-slider',
+   });
+
+   $('.header__text-slider').slick({
+       asNavFor: '.header__slider',
+       arrows: false
    });
 
    $('.certificates__slider').slick({
