d8efa90f29f91f17c09f1e8fed153b95b0d4ec81
Denys Petrushkov <denis.p@cheitgroup.com>
2020-02-26 09:37:22 +0200

    added plugins

--- js/main.js
+++ js/main.js
@@ -1,6 +1,14 @@
 jQuery(document).ready(function($) {
     
-   
+   $('.header__slider').slick({
+       arrows: true
+   });
+
+   $('.certificates__slider').slick({
+       arrows: true,
+       slidesToShow: 3,
+       slidesToScroll: 1
+   })
 
 });
 
